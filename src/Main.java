d11ed4c349f6395c80cda2fda68d44e82d8934a0
chandlercarter <cartecl@mail.uc.edu>
2023-06-27 14:13:35 -0400

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+    Scanner in = new Scanner(System.in);
+    int age = 0;    //num age = 0
+
+    System.out.print("Input your age");         // output "input your age"
+    in.hasNextInt();                            // input age
+    if (in.hasNextInt()) {
+        age = in.nextInt();
+        if (age >= 21) {                        // if age >= 21 then
+        System.out.println("You are 21 or over, you get a wristband."); //output "You are 21 or over, you get a wristband"
+        }
+    } //end if
+    } //return
+} //end class
